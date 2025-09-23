34534713723f0eda859feef46c93a6ab2c92f2c8
ken-arthur005 <careyckenarthur@gmail.com>
2025-09-23 18:55:29 +0000

    the initial ui

--- blast-simulation-game/src/App.jsx
+++ blast-simulation-game/src/App.jsx
@@ -1,12 +1,12 @@
-
+import MainUi from './Pages/HomePage'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl font-extrabold text-blue-950'>WELCOME TO OUR MINERAL BLASTING SIMULATOR</h1>
-      
+      <MainUi/>
     </>
   )
 }
--- blast-simulation-game/src/main.jsx
+++ blast-simulation-game/src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import HomePage from './Pages/HomePage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
