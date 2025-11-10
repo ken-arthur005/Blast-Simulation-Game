f26fe61813ee12f780d9a7b7c19c41bb2a54ecf1
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-11-10 10:53:49 +0000

    Create buildNormalizationStats()
    buldNormalizationStats() loop through affected cells and returns an object of all
    min max values of all the numberic properties of the affected cells

--- blast-simulation-game/src/utils/recoveryCalculator.js
+++ blast-simulation-game/src/utils/recoveryCalculator.js
@@ -3,3 +3,34 @@ import {
   safeMinMaxNormalize,
   logTransform,
 } from "./normalization";
+
+/**
+ * Build min/max stats for affected cells (per-blast scope)
+ * grid: 2D grid array with cell props
+ * affectedCells: array of {x, y, distance, ...}
+ */
+export function buildNormalizationStats(affectedCells, grid, options = {}) {
+  const useLogDensity = options.useLogDensity === true;
+
+  const densities = [];
+  const hardnesses = [];
+  const fragmentations = [];
+  const distances = [];
+
+  affectedCells.forEach((c) => {
+    const cell = (grid?.[c.y] || [])[c.x] || {};
+    const rawDensity = Number(cell.density ?? NaN);
+    densities.push(useLogDensity ? logTransform(rawDensity) : rawDensity);
+    hardnesses.push(Number(cell.hardness ?? NaN));
+    fragmentations.push(Number(cell.fragmentation_index ?? NaN));
+    distances.push(Number(c.distance ?? NaN));
+  });
+
+  return {
+    densityStats: minMaxStats(densities),
+    hardnessStats: minMaxStats(hardnesses),
+    fragmentationStats: minMaxStats(fragmentations),
+    distanceStats: minMaxStats(distances),
+    useLogDensity,
+  };
+}
